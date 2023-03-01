7d3fafd74fbeb1a115efebfe76cd107693cd2dd9
u0733172 <u0733172@users.noreply.github.com>
2023-03-01 19:01:11 +0100

    Update app.py

--- OCProjet7/app/app.py
+++ OCProjet7/app/app.py
@@ -16,7 +16,7 @@ sns.set_style('darkgrid')
 
 
 def main():
-    @st.cache
+    @st.cache_data
     def load_data():
         z = ZipFile("data/default_risk.zip")
         data = pd.read_csv(z.open('default_risk.csv'), index_col='SK_ID_CURR', encoding='utf-8')
@@ -42,7 +42,7 @@ def main():
         knn = knn_training(sample)
         return knn
 
-    @st.cache
+    @st.cache_data
     def load_infos_gen(data):
         lst_infos = [data.shape[0],
                      round(data["AMT_INCOME_TOTAL"].mean(), 2),
@@ -60,24 +60,24 @@ def main():
         data_client = data[data.index == int(id)]
         return data_client
 
-    @st.cache
+    @st.cache_data
     def load_age_population(data):
         data_age = round((data["DAYS_BIRTH"] / 365), 2)
         return data_age
 
-    @st.cache
+    @st.cache_data
     def load_income_population(sample):
         df_income = pd.DataFrame(sample["AMT_INCOME_TOTAL"])
         df_income = df_income.loc[df_income['AMT_INCOME_TOTAL'] < 200000, :]
         return df_income
 
-    @st.cache
+    @st.cache_data
     def load_prediction(sample, id, clf):
         X = sample.iloc[:, :-1]
         score = clf.predict_proba(X[X.index == int(id)])[:, 1]
         return score
 
-    @st.cache
+    @st.cache_data
     def load_kmeans(sample, id, mdl):
         index = sample[sample.index == int(id)].index.values
         index = index[0]
@@ -86,7 +86,7 @@ def main():
         df_neighbors = pd.concat([df_neighbors, data], axis=1)
         return df_neighbors.iloc[:, 1:].sample(10)
 
-    @st.cache
+    @st.cache_data
     def knn_training(sample):
         knn = KMeans(n_clusters=2).fit(sample)
         return knn
